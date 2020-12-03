a3a5bfcee2ae37d5d2608e41fb9e39f9fead3aa0
Kyungyun Kim <kyknn79@naver.com>
2020-12-04 01:40:11 +0900

    information toolbar 홈버튼 생성

--- app/src/main/java/com/example/beesang/Medical_information.java
+++ app/src/main/java/com/example/beesang/Medical_information.java
@@ -1,7 +1,14 @@
 package com.example.beesang;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class Medical_information extends AppCompatActivity {
 
@@ -9,5 +16,27 @@ public class Medical_information extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medical_information);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    // 밑에는 툴바에 홈버튼 만드는 함수들
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu:
+                Intent NewActivity = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(NewActivity);
+                break;
+        }
+        return true;
     }
 }
