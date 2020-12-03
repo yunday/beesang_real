a3645db2b31c4dcccde0a107599fb9505e28917d
Kyungyun Kim <kyknn79@naver.com>
2020-12-04 01:34:18 +0900

    contact 기능 완료

--- app/src/main/java/com/example/beesang/ContactActivity.java
+++ app/src/main/java/com/example/beesang/ContactActivity.java
@@ -5,7 +5,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -19,7 +21,11 @@ import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class ContactActivity extends AppCompatActivity {
-    String number_tmp = "";
+    EditText et_1, et_2, et_3;
+    private SharedPreferences sp;
+    SharedPreferences.Editor editor;
+    String name1, name2, name3;
+    String number1, number2, number3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +51,10 @@ public class ContactActivity extends AppCompatActivity {
         final EditText et_name1  = findViewById(R.id.et_name1);
         final EditText et_name2  = findViewById(R.id.et_name2);
         final EditText et_name3  = findViewById(R.id.et_name3);
-        final EditText et_1 = findViewById(R.id.et_1);
-        final EditText et_2 = findViewById(R.id.et_2);
-        final EditText et_3 = findViewById(R.id.et_3);
+        et_1 = findViewById(R.id.et_1);
+        et_2 = findViewById(R.id.et_2);
+        et_3 = findViewById(R.id.et_3);
+        Button btn_save_real = findViewById(R.id.btn_save);
 
         btn_edit1.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -115,7 +122,6 @@ public class ContactActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
                 startActivityForResult(intent, 0);
-                et_1.setText(number_tmp);
             }
         });
         btn_contact2.setOnClickListener(new View.OnClickListener(){
@@ -123,8 +129,7 @@ public class ContactActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
-                startActivityForResult(intent, 0);
-                et_2.setText(number_tmp);
+                startActivityForResult(intent, 1);
             }
         });
         btn_contact3.setOnClickListener(new View.OnClickListener(){
@@ -132,10 +137,31 @@ public class ContactActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(Intent.ACTION_PICK);
                 intent.setData(ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
-                startActivityForResult(intent, 0);
-                et_3.setText(number_tmp);
+                startActivityForResult(intent, 2);
             }
         });
+        btn_save_real.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                editor.putString("name1", tv_name1.getText().toString());
+                editor.putString("name2", tv_name2.getText().toString());
+                editor.putString("name3", tv_name3.getText().toString());
+                editor.putString("number1", et_1.getText().toString());
+                editor.putString("number2", et_2.getText().toString());
+                editor.putString("number3", et_3.getText().toString());
+                editor.apply();
+            }
+        });
+
+        sp = getSharedPreferences("myFile", Activity.MODE_PRIVATE);
+        editor = sp.edit();
+
+        tv_name1.setText(sp.getString("name1", "보호자1"));
+        tv_name2.setText(sp.getString("name2", "보호자2"));
+        tv_name3.setText(sp.getString("name3", "보호자3"));
+        et_1.setText(sp.getString("number1", ""));
+        et_2.setText(sp.getString("number2", ""));
+        et_3.setText(sp.getString("number3", ""));
     }
 
     @Override
@@ -149,7 +175,12 @@ public class ContactActivity extends AppCompatActivity {
                                 ContactsContract.CommonDataKinds.Phone.NUMBER}, null, null, null); }
             if (cursor != null) {
                 cursor.moveToFirst();
-                number_tmp = cursor.getString(1);
+                if(requestCode==0)
+                    et_1.setText(cursor.getString(1));
+                else if(requestCode==1)
+                    et_2.setText(cursor.getString(1));
+                else if(requestCode==2)
+                    et_3.setText(cursor.getString(1));
                 cursor.close();
             }
         }
