3ee587db74d9fc021b97db984c9d9e2ad8ec5be6
dbwls387 <dbwls387@naver.com>
2020-12-04 15:21:16 +0900

    지니 메인

--- app/src/main/java/com/example/beesang/MainActivity.java
+++ app/src/main/java/com/example/beesang/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.beesang;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,18 +15,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//
-//        Button guide_btn = findViewById(R.id.setting_guide);
-//        guide_btn.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(
-//                        getApplicationContext(), Setting_guide.class );
-//                startActivity(intent);
-//            }
-//        });
 
-        Button button = findViewById(R.id.btn_setting);
+        Button guide_btn = findViewById(R.id.btn_guide);
+        guide_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), Setting_guide.class );
+                startActivity(intent);
+            }
+        });
+
+        Button button = findViewById(R.id.btn_button);
         button.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -40,25 +35,25 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-//
-//        Button contact = findViewById(R.id.contact);
-//        contact.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(
-//                        getApplicationContext(), ContactActivity.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//        Button medical = findViewById(R.id.medical);
-//        medical.setOnClickListener(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(
-//                        getApplicationContext(), Medical_information.class);
-//                startActivity(intent);
-//            }
-//        });
+
+        Button contact = findViewById(R.id.contact);
+        contact.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), ContactActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button medical = findViewById(R.id.medical);
+        medical.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(
+                        getApplicationContext(), Medical_information.class);
+                startActivity(intent);
+            }
+        });
     }
 }
